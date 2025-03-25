448a2f18b966214899282f10553eb3b09d8eea7b
Luis Gabriel Garcia Valdes <dev.nether@gmail.com>
2025-03-25 18:17:52 -0400

    Finished secondChanceItemsRoutes service

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -6,13 +6,14 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-
-
 const app = express();
-app.use("*",cors());
 const port = 3060;
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
 
-// Connect to MongoDB; we just do this one time
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
+
+app.use("*",cors());
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
@@ -20,40 +21,13 @@ connectToDatabase().then(() => {
 
 
 app.use(express.json());
-
-// Route files
-
-// authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
-//{{insert code here}}
-
-// Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
-
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
-
 app.use(pinoHttp({ logger }));
-
-// Use Routes
-// authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
-//{{insert code here}}
-
-// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
-
-// Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
+
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 app.get("/",(req,res)=>{
     res.send("Inside the server")
